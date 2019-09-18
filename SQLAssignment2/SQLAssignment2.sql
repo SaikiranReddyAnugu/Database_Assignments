623a964bf5330c68e1ac6937ac96abaeaf51c0e7
SaikiranReddyAnugu <52755279+SaikiranReddyAnugu@users.noreply.github.com>
2019-09-18 11:05:02 +0530

    final Commit

--- SQLAssignment2/SQLAssignment2.sql
+++ SQLAssignment2/SQLAssignment2.sql
@@ -1,13 +1,13 @@
 1.select count(*) as Documentaries_with_Deleted_scenes from film f inner join film_category fc on f.film_id=fc.film_id
  and f.special_features  like '%deleted Scenes%' inner join category c on c.category_id=fc.category_id and c.name="Documentary";
  
-2.select s.*,count(distinct f.film_id) as Count_of_sci_fi_movies from store s 
- inner join staff st on s.manager_staff_id=st.staff_id 
+2.select count(distinct f.film_id) as Count_of_sci_fi_movies from rental r 
+ inner join staff st on r.staff_id=st.staff_id 
  and st.first_name="Jon" and st.last_name="Stephens" 
- inner join inventory i on s.store_id=i.store_id 
+ inner join inventory i on r.inventory_id=i.inventory_id 
  inner join film f on f.film_id=i.film_id
  inner join film_category fc on f.film_id=fc.film_id  
- inner join category ca on ca.category_id=fc.category_id and ca.name="sci-fi" group by s.store_id;
+ inner join category ca on ca.category_id=fc.category_id and ca.name="sci-fi";
  
 3.select * from sales_by_film_category where category="Animation";
  
